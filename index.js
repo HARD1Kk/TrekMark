03a9f4c7c0833bfbbae4287d1ecb141cb3c0c1bf
Hardik <Hardik21673@gmail.com>
2024-11-26 15:35:34 +0530

    Initial commit: Add Node.js application with PostgreSQL integration

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-import express from "express";
-import bodyParser from "body-parser";
-import pg from "pg";
+import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import pkg from 'pg';
+const { Client } = pkg;
+dotenv.config(); // Load environment variables from .env file
+
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "myWorld",
-  password: "jain@@1234",
-  port: "6969"
-})
+const db = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 
 db.connect();
@@ -46,7 +48,7 @@ app.post("/add", async (req, res) => {
   const countryName = req.body.country;
   console.log(countryName);
   try {
-    const result = await db.query("SELECT country_code from countries WHERE LOWER(country_name) LIKE '%' || $1 || '%' ; ",[ countryName.toLowerCase()]);
+    const result = await db.query("SELECT country_code from countries WHERE LOWER(country_name) LIKE '%' || $1 || '%' ; ",[ countryName.toLowerCase() ]);
       // Checking if that country exists in the database 
     //here we get the country code from the user entered country name
 
